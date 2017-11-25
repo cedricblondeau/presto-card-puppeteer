ce6dcd5392d91edd263974a3d58ccf2228939602
Cédric Blondeau <hi@cedricblondeau.com>
2017-11-25 14:22:54 -0500

    Add getBalance function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
 const puppeteer = require('puppeteer');
 
-(async() => {
-    const browser = await puppeteer.launch();
-    console.log(await browser.version());
-    browser.close();
-})();
+const loginPageUrl = 'https://www.prestocard.ca/en-US/Pages/TransactionalPages/AccountLogin.aspx';
+const loginPageUsernameInputSelector = 'input[id=SignIn_Username]';
+const loginPagePasswordInputSelector = 'input[id=SignIn_Password]';
+const loginPageSubmitSelector = 'button[id=btnsubmit]';
+const dashboardBalanceSelector = 'p.dashboard__quantity';
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+
+async function getBalance(username, password) {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  page.setUserAgent(userAgent);
+  await page.goto(loginPageUrl, { waitUntil: 'load' });
+
+  await page.waitFor(loginPageUsernameInputSelector);
+  await page.type(loginPageUsernameInputSelector, username);
+
+  await page.waitFor(loginPagePasswordInputSelector);
+  await page.type(loginPagePasswordInputSelector, password);
+
+  await page.click(loginPageSubmitSelector);
+
+  await page.waitForSelector(dashboardBalanceSelector);
+
+  const balance = await page.evaluate(body => body.getElementsByClassName('dashboard__quantity')[0].innerText);
+  browser.close();
+
+  return balance;
+}
+
+module.exports = {
+  getBalance,
+};
